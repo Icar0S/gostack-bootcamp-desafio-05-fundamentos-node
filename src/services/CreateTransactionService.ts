ea7b8142222616f14ca034f1d74584267dc410aa
Icaro Santos <Icarooliveira_23@hotmail.com>
2020-07-07 04:03:31 -0300

    tests

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import { response } from 'express';
 
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === "outcome" && total < value) {
+      throw new Error("Não há saldo sufuciente!");
+    }
+
+    const transactions = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    return transactions;
   }
 }
 
